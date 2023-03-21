fd5ff09485cfb9b269bba64464bfe5a268f21ea3
Deniz Cansu Turan <denizcansuturan@gmail.com>
2023-03-21 03:12:18 +0300

    Add files via upload

---
+++ arl.py
@@ -0,0 +1,262 @@
+############################################
+# ASSOCIATION RULE LEARNING
+############################################
+
+# 1. Data Preprocessing
+# 2. Preparing ARL Data Structure (Invoice-Product Matrix)
+# 3. Association Rules
+# 4. Preparing the Script of the Work
+# 5. Preparing the Script of the Work
+
+############################################
+# 1. Data Preprocessing
+############################################
+
+# !pip install mlxtend
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 500)
+# ensures that the output is on a single line
+pd.set_option('display.expand_frame_repr', False)
+from mlxtend.frequent_patterns import apriori, association_rules
+
+# https://archive.ics.uci.edu/ml/datasets/Online+Retail+II
+
+df_ = pd.read_excel("D:/MIUUL/RECOMMENDATİON SYSTEMS/recommender_systems/datasets/online_retail_II.xlsx",
+                    sheet_name="Year 2009-2010")
+df = df_.copy()
+
+df.head()
+# pip install openpyxl
+# df_ = pd.read_excel("datasets/online_retail_II.xlsx",
+#                     sheet_name="Year 2010-2011", engine="openpyxl")
+
+df.describe().T
+df.isnull().sum()
+df.shape
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    return dataframe
+
+df = retail_data_prep(df)
+
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe["Invoice"] = dataframe["Invoice"].astype(str)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    return dataframe
+
+df = retail_data_prep(df)
+df.isnull().sum()
+df.describe().T
+df.shape
+
+############################################
+# 2. Preparing ARL Data Structure (Invoice-Product Matrix)
+############################################
+
+df.head()
+
+# wanted ARL Data Structure (Invoice-Product Matrix)(2012-2011)
+# Description   NINE DRAWER OFFICE TIDY   SET 2 TEA TOWELS I LOVE LONDON    SPACEBOY BABY GIFT SET
+# Invoice
+# 536370                              0                                 1                       0
+# 536852                              1                                 0                       1
+# 536974                              0                                 0                       0
+# 537065                              1                                 0                       0
+# 537463                              0                                 0                       1
+
+
+df_fr = df[df['Country'] == "France"]
+
+df_fr.groupby(['Invoice', 'Description']).agg({"Quantity": "sum"}).head(20)
+# to see what are the products and their quantities in an invoice
+
+df_fr.groupby(['Invoice', 'Description']).agg({"Quantity": "sum"}).unstack().iloc[0:5, 0:5]
+# iloc is used to select index-based rows and columns
+
+df_fr.groupby(['Invoice', 'Description']).agg({"Quantity": "sum"}).unstack().fillna(0).iloc[0:5, 0:5]
+# null values are replaced with 0
+
+df_fr.groupby(['Invoice', 'StockCode']). \
+    agg({"Quantity": "sum"}). \
+    unstack(). \
+    fillna(0). \
+    applymap(lambda x: 1 if x > 0 else 0).iloc[0:5, 0:5]
+
+# applymap applies a function on every observation cell.
+
+# StockCodes are unique for every product(description)
+
+# Using stockcodes improves speed and performance,
+# however it decreases readibility.
+
+# Therefore, a function is defined to let the user choose between
+# stockcode and describtion
+
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.groupby(['Invoice', "StockCode"])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.groupby(['Invoice', 'Description'])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+
+fr_inv_pro_df = create_invoice_product_df(df_fr)
+# column numbers are higher with descriptions,
+# so some product do not have stockcodes
+
+fr_inv_pro_df = create_invoice_product_df(df_fr, id=True)
+
+create_invoice_product_df(df_fr).iloc[0:6, 0:4]
+create_invoice_product_df(df_fr, id=True).iloc[0:6, 0:4]
+
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
+    print(product_name)
+
+
+check_id(df_fr, 10002)
+# to check what the description of a stockcode is
+
+############################################
+# 3. Association Rules Analysis
+############################################
+
+frequent_itemsets = apriori(fr_inv_pro_df.astype('bool'),
+                            min_support=0.015,
+                            use_colnames=True)
+
+frequent_itemsets.sort_values("support", ascending=False).head()
+
+rules = association_rules(frequent_itemsets,
+                          metric="support",
+                          min_threshold=0.01)
+rules.head()
+rules[(rules["support"]>0.05) & (rules["confidence"]>0.1) & (rules["lift"]>5)]
+
+check_id(df_fr, 22356)
+
+rules[(rules["support"]>0.05) & (rules["confidence"]>0.1) & (rules["lift"]>5)].sort_values("confidence", ascending=False)
+
+############################################
+# 4. Preparing the Script of the Work
+############################################
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+def retail_data_prep(dataframe):
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    return dataframe
+
+
+def create_invoice_product_df(dataframe, id=False):
+    if id:
+        return dataframe.groupby(['Invoice', "StockCode"])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+    else:
+        return dataframe.groupby(['Invoice', 'Description'])['Quantity'].sum().unstack().fillna(0). \
+            applymap(lambda x: 1 if x > 0 else 0)
+
+
+def check_id(dataframe, stock_code):
+    product_name = dataframe[dataframe["StockCode"] == stock_code][["Description"]].values[0].tolist()
+    print(product_name)
+
+
+def create_rules(dataframe, id=True, country="France"):
+    dataframe = dataframe[dataframe['Country'] == country]
+    dataframe = create_invoice_product_df(dataframe, id)
+    frequent_itemsets = apriori(dataframe.astype('bool'), min_support=0.015, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric="support", min_threshold=0.01)
+    return rules
+
+df = df_.copy()
+
+df = retail_data_prep(df)
+rules = create_rules(df)
+
+rules[(rules["support"]>0.05) & (rules["confidence"]>0.1) & (rules["lift"]>5)]. \
+sort_values("confidence", ascending=False)
+
+############################################
+# 5. Product recommendations to users at the shopping cart stage
+############################################
+
+product_id = 22492 # let's say a customer added product 22492 to her shopping cart
+check_id(df, product_id) # description
+
+sorted_rules = rules.sort_values("lift", ascending=False)
+# rules are sorted wrt lift, we could sort them wrt confidence or suuport
+# whichever is important to you
+recommendation_list = []
+
+for i, product in enumerate(sorted_rules["antecedents"]):
+    for j in list(product):
+        if j == product_id:
+            recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
+# we need both the index and the antecedents code so we used enumerate
+# since multiple codes are stored in a frozenset we had to change it to a list
+# if the product code is the one user added to cart, we add the corresponding
+# consequents code to the user with the help of index
+# this is a index search so we need to use iloc
+recommendation_list[0:3]
+
+def arl_recommender(rules_df, product_id, rec_count=1):
+    sorted_rules = rules_df.sort_values("lift", ascending=False)
+    recommendation_list = []
+    for i, product in enumerate(sorted_rules["antecedents"]):
+        for j in list(product):
+            if j == product_id:
+                recommendation_list.append(list(sorted_rules.iloc[i]["consequents"])[0])
+
+    return recommendation_list[0:rec_count]
+
+
+arl_recommender(rules, 22492, 1)
+arl_recommender(rules, 22492, 2)
+arl_recommender(rules, 22492, 3)
+
+
+
+
+
